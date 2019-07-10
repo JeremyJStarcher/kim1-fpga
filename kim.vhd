bd145a40ae5c40cebc0e790bb0a5ac3aeabbba2a
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-07-10 01:45:01 -0400

    Two separate clocks functioning

--- kim.vhd
+++ kim.vhd
@@ -18,17 +18,11 @@ port (
 end Kim;
 
 architecture rtl of Kim is
-	constant CLK_FREQ : integer :=   50000000;
-	constant BLINK_FREQ : integer := 500000;
-	constant CNT_MAX : integer := CLK_FREQ/BLINK_FREQ/2-1;
-	
-	signal cnt		 : unsigned(24 downto 0);
 	signal max_d 	: std_logic_vector(31 downto 0) := "00011111111111111111111111111110";
-
-	signal blink	: std_logic;
-
 	signal Q1		: std_logic_vector(3 downto 0);	
 	signal reset	: std_logic;
+	signal spi_clock : std_logic;
+	signal tick_clock : std_logic;
 	
 component Bit4 is
 	port(C, CLR : in std_logic;
@@ -45,43 +39,47 @@ component M7219 is
     );
 end component;
 
+component Clock is
+	port(C, CLR : in std_logic;
+	spi_clock : out std_logic;
+	tick_clock : out std_logic);
+end component;
 	
 begin
-	div1: Bit4 port map (C => blink, CLR => reset, Q => Q1);
+	c1: Clock port map (C => clk, CLR => reset, spi_clock => spi_clock, tick_clock => tick_clock);
+	div1: Bit4 port map (C => tick_clock, CLR => reset, Q => Q1);
 
-	max1: M7219 port map (clk => blink,
+	max1: M7219 port map (clk => spi_clock,
 		parallel => max_d,
 		clk_out => max_clk,
 		load => max_cs,
 		data_out => max_din 
 	);
 	
-	led2 <= '1';
-	led1 <= '1';
-	-- led0 <= '1';
 	
-	--led2 <= not Q1(0);
-	--led1 <= not Q1(1);
-	--led0 <= not Q1(2);
-		
+	led2 <= not Q1(0);
+	led1 <= not Q1(1);
+	led0 <= not Q1(2);
+
+	process(tick_clock)
+	begin
+		if (reset = '1') then
+			max_d <= (others => '0');
+		elsif rising_edge(tick_clock) then
+			max_d <= max_d + 1;
+		end if;
+
+	end process;
+	
 	process(clk, sw0)
 	
 	begin
 	
-	if rising_edge(clk) then
-		
+		if rising_edge(clk) then
 			if sw0='0' then
 				reset <= '1';
 			else
 				reset <= '0';
-			end if;
-
-			if cnt >= CNT_MAX then
-				cnt <= (others => '0');
-				blink <= not blink;
-			else
-			   max_d <= max_d + 1;
-				cnt <= cnt + 1;
 			end if;
 		end if;
 
